d8aa107f681a271c66d41c32f75f2199045fb8f3
aqsa <aqsa@xpertise.ae>
2023-11-12 21:20:05 +0400

    home page model issue

--- app/(frontend)/components/Project/ProjectList.tsx
+++ app/(frontend)/components/Project/ProjectList.tsx
@@ -1,19 +1,102 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef, useId } from "react";
 import React from "react";
 import Select from "react-select";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-
+import {
+  GoogleMap,
+  InfoWindow,
+  MarkerF,
+  useLoadScript,
+  OverlayView,
+} from "@react-google-maps/api";
 import { useGetAllHomeData } from "@/src/services/HomeService";
+import Modal from "./Model";
+import MapProject from "./MapProject";
+
+import Property from "../Property/Property";
+
 function ProjectList() {
   const router = useRouter();
   const { homeData } = useGetAllHomeData();
-  const [selectedProject, setSelectedProjectName] = useState();
+
   const options = homeData?.newProjects;
   const projectChangeHandle = (event) => {
-    console.log(event.value);
     router.push("/projects/" + event.value);
+  };
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.GOOGLE_MAP_KEY,
+  });
+  const [mapRef, setMapRef] = useState();
+  const [isOpen, setIsOpen] = useState(false);
+  const [infoWindowData, setInfoWindowData] = useState();
+  const markers = homeData?.mapProjects;
+  const [minMaxPrice, setMinMaxPrice] = useState({
+    minPrice: 0,
+    maxPrice: 0,
+  });
+
+  const minPriceRef = useRef(null);
+  const maxPriceRef = useRef(null);
+
+  const handleApplyPrice = () => {
+    setMinMaxPrice({
+      minPrice: minPriceRef.current.value,
+      maxPrice: maxPriceRef.current.value,
+    });
+    const minPrice = minPriceRef.current.value ? minPriceRef.current.value : 0;
+    const maxPrice = maxPriceRef.current.value ? maxPriceRef.current.value : 0;
+
+    router.push(`rent?minprice=${minPrice}&maxprice=${maxPrice}`);
+  };
+  const onMapLoad = (map) => {
+    setMapRef(map);
+    const bounds = new google.maps.LatLngBounds();
+    markers?.forEach(({ lat, lng }) => bounds.extend({ lat, lng }));
+    map.fitBounds(bounds);
+  };
+
+  const handleMarkerClick = (
+    id,
+    lat,
+    lng,
+    address,
+    name,
+    area,
+    bedrooms,
+    bathrooms,
+    price,
+    property_banner,
+    slug
+  ) => {
+    mapRef?.panTo({ lat, lng });
+    setInfoWindowData({
+      id,
+      address,
+      name,
+      area,
+      bedrooms,
+      bathrooms,
+      price,
+      property_banner,
+    });
+    setIsOpen(true);
+  };
+  const [selectedProject, setSelectedProject] = useState({
+    id: "",
+    label: "",
+    value: "",
+  });
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
   };
   return (
     <>
@@ -111,7 +194,6 @@ function ProjectList() {
           </div>
         </div>
       </section>
-
       <div
         className="modal fade"
         id="exampleModal"
@@ -132,18 +214,95 @@ function ProjectList() {
                 aria-label="Close"
               ></button>
             </div>
-            <div className="modal-body">...</div>
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-bs-dismiss="modal"
-              >
-                Close
-              </button>
-              <button type="button" className="btn btn-primary">
-                Save changes
-              </button>
+            <div className="modal-body">
+              <div className="">
+                {!isLoaded ? (
+                  <h1>Loading...</h1>
+                ) : (
+                  <GoogleMap
+                    mapContainerClassName="map-container"
+                    onLoad={onMapLoad}
+                    onClick={() => setIsOpen(false)}
+                  >
+                    {markers.map(
+                      (
+                        {
+                          address,
+                          name,
+                          area,
+                          bedrooms,
+                          bathrooms,
+                          price,
+                          property_banner,
+                          lat,
+                          lng,
+                          slug,
+                        },
+                        ind
+                      ) => (
+                        <MarkerF
+                        key={ind}
+                        position={{ lat, lng }}
+                        onClick={() => {
+                          handleMarkerClick(
+                            ind,
+                            lat,
+                            lng,
+                            address,
+                            name,
+                            area,
+                            bedrooms,
+                            bathrooms,
+                            price,
+                            property_banner,
+                            slug
+                          );
+                        }}
+                      >
+                          <OverlayView
+                          position={{ lat, lng }}
+                          mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+                        >
+                          <div
+                            style={{
+                              backgroundColor: "white",
+                              padding: "5px",
+                              border: "1px solid #ccc",
+                              boxShadow: "0 2px 6px rgba(0, 0, 0, 0.3)",
+                              borderRadius: "4px",
+                              minWidth: "50px", // Set a minimum width
+                              whiteSpace: "nowrap", // Rounded corners
+                            }}
+                          >
+                            {price}
+                          </div>
+                        </OverlayView>
+                        {isOpen && infoWindowData?.id === ind && (
+                          <InfoWindow
+                            onCloseClick={() => {
+                              setIsOpen(false);
+                            }}
+                          >
+                            <div>
+                              <Property
+                                slug={infoWindowData.slug}
+                                area={infoWindowData.area}
+                                bathrooms={infoWindowData.bathrooms}
+                                bedrooms={infoWindowData.bedrooms}
+                                price={infoWindowData.price}
+                                address={infoWindowData.address}
+                                property_banner={infoWindowData.property_banner}
+                                name={infoWindowData.name}
+                              />
+                            </div>
+                          </InfoWindow>
+                        )}
+                        </MarkerF>
+                      )
+                    )}
+                  </GoogleMap>
+                )}
+              </div>
             </div>
           </div>
         </div>
--- app/(frontend)/components/Project/SingleProjectView.tsx
+++ app/(frontend)/components/Project/SingleProjectView.tsx
@@ -12,6 +12,10 @@ import parse from "html-react-parser";
 // import GoogleMapReact from "google-map-react";
 import { useMemo } from "react";
 import { useGetSingleProjectData } from "@/src/services/ProjectService";
+
+import { toast } from "react-toastify";
+import { saveContactFormApi } from "@/src/services/HomeService";
+
 import "@/public/css/single-project-view-styles.css";
 import "@/public/css/responsive.css";
 
@@ -24,6 +28,43 @@ function SingleProjectView({ params }) {
   const hightlightSwiperRef = useRef<SwiperCore>();
   const rentSwiperRef = useRef<SwiperCore>();
   const saleSwiperRef = useRef<SwiperCore>();
+
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+    phone: "",
+    formName: "enquireForm",
+    page: "services",
+  });
+  const contactCloseRef = useRef(null);
+
+  const handleSubmit = () => {
+    if (!formData.name || !formData.email || !formData.phone) {
+      return toast.error("Please fill required field");
+    }
+    saveContactFormApi(formData)
+      .then((res) => {
+        toast.success(
+          "Enquire form submitted successfully, out support teams contact you soon"
+        );
+        contactCloseRef.current.click();
+        setFormData({
+          name: "",
+          email: "",
+          subject: "",
+          message: "",
+          phone: "",
+          formName: "enquireForm",
+          page: "services",
+        });
+      })
+      .catch((err) => {
+        toast.error("Something went wrong, please try again");
+      });
+  };
 
   // const [thumbsSwiper, setThumbsSwiper] = useState(null);
   return (
@@ -286,7 +327,7 @@ function SingleProjectView({ params }) {
                         <button
                           className="fillBtn tblBtn mrAuto"
                           data-bs-toggle="modal"
-                          data-bs-target={"#pricePlaneModal" + type.id}
+                          data-bs-target={"#pricePlanModal" + type.id}
                         >
                           view
                         </button>
@@ -295,7 +336,7 @@ function SingleProjectView({ params }) {
                         <button
                           className="fillBtn tblBtn mrAuto"
                           data-bs-toggle="modal"
-                          data-bs-target="#floorPlaneModal"
+                          data-bs-target={"#floorPlanModal" + type.id}
                         >
                           view
                         </button>
@@ -305,7 +346,6 @@ function SingleProjectView({ params }) {
                 })}
               </tbody>
             </table>
-
           </div>
         </div>
       </section>
@@ -624,8 +664,8 @@ function SingleProjectView({ params }) {
         return (
           <div
             className="modal fade"
-            id={"pricePlaneModal" + type.id}
-            key={index + "pricePlaneModal"}
+            id={"pricePlanModal" + type.id}
+            key={index + "pricePlanModal"}
           >
             <div className="modal-dialog modal-lg">
               <div className="modal-content">
@@ -638,6 +678,7 @@ function SingleProjectView({ params }) {
                     aria-label="Close"
                   ></button>
                 </div>
+                
                 <div className="modal-body">
                   <div className="labelFLex">
                     <label className="priceLabel">
@@ -685,6 +726,171 @@ function SingleProjectView({ params }) {
               </div>
             </div>
           </div>
+        );
+      })}
+
+      {projectData?.types?.map((type, index) => {
+        return (
+         
+
+<div
+className="modal fade"
+id={"floorPlanModal" + type.id}
+key={index + "floorPlanModal"}
+tabIndex={-1}
+aria-labelledby="exampleModalLabel"
+aria-hidden="true"
+ref={contactCloseRef}
+>
+<div className="modal-dialog  modal-dialog-centered modal-lg modalBookMeet ">
+  <div className="modal-content">
+    <div className="modal-header border-0 justify-content-end p-1">
+      <button
+        type="button"
+        className="bg-transparent border-0"
+        data-bs-dismiss="modal"
+        aria-label="Close"
+      >
+        <i className="bi bi-x-circle text-primary"></i>
+      </button>
+    </div>
+
+    <div className="modal-body  p-0 rounded-1 m-2">
+      <div className="row g-0">
+        <div className="col-12 col-lg-5 col-md-12 border-end descricalenderCol">
+          <div className="border-bottom">
+            <div className="p-3">
+              <img
+                src="/images/logo_blue.png"
+                alt="Range Property"
+                className="img-fluid"
+                width="150"
+              />
+            </div>
+          </div>
+          <div className="p-3">
+            <div className="assist-text  text-left mt-3 mb-5">
+              <h5>
+                <strong className="need">An esteemed award-winning real estate brokerage based in Dubai, UAE</strong>
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-lg-7 col-md-12 ">
+          <div className=" p-4">
+            <form action="" method="POST">
+              <div className="">
+                <div className="row">
+                  <div className="col-md-12">
+                    <h6 className="text-primary">Enter Details</h6>
+                    <div className="form-group">
+                      <label>
+                        Name<small className="text-danger">*</small>
+                      </label>
+                      <input
+                        type="text"
+                        name="nameCon2"
+                        id="nameCon2"
+                        className="form-control mb-2"
+                        placeholder="Enter your name"
+                        autoComplete="off"
+                        value={formData.name}
+                        onChange={(e) =>
+                          setFormData({
+                            ...formData,
+                            name: e.target.value,
+                          })
+                        }
+                        required
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label>
+                        Email<small className="text-danger">*</small>
+                      </label>
+                      <input
+                        type="email"
+                        name="emailCon2"
+                        id="emailCon2"
+                        className="form-control mb-2"
+                        placeholder="Enter your email"
+                        autoComplete="off"
+                        value={formData.email}
+                        onChange={(e) =>
+                          setFormData({
+                            ...formData,
+                            email: e.target.value,
+                          })
+                        }
+                        required
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label>
+                        Phone Number{" "}
+                        <small className="text-danger">*</small>
+                      </label>
+                      <input
+                        id="fullNumber3"
+                        type="hidden"
+                        name="fullNumber"
+                      />
+                      <input
+                        type="tel"
+                        className="form-control mb-2"
+                        id="telephoneNew3"
+                        name="phone"
+                        placeholder="Enter your Phone Number"
+                        value={formData.phone}
+                        onChange={(e) =>
+                          setFormData({
+                            ...formData,
+                            phone: e.target.value,
+                          })
+                        }
+                        autoComplete="off"
+                        required
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label>Message</label>
+                      <input
+                        type="text"
+                        name="messageCon2"
+                        id="messageCon2"
+                        className="form-control mb-2"
+                        placeholder="Message"
+                        autoComplete={"off"}
+                        value={formData.message}
+                        onChange={(e) =>
+                          setFormData({
+                            ...formData,
+                            message: e.target.value,
+                          })
+                        }
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="modal-footer border-0">
+                  <button
+                    type="submit"
+                    name="submit"
+                    className="btn btn-blue rounded-0 px-5 float-end btnContact2"
+                    onClick={handleSubmit}
+                  >
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
         );
       })}
 
